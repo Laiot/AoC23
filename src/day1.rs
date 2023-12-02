96681dddc505e44b5625df33d95e5c6e317113d2
Carmelo Sarta <carmelo.sarta.main@gmail.com>
2023-12-02 23:50:34 +0100

    Day 1 Part 1 prettier

--- src/day1.rs
+++ src/day1.rs
@@ -6,13 +6,9 @@ pub fn part1(input: &str) -> u32{
     input.lines()
 		.map(|line| {
 			let mut line = line.chars().filter_map(|c| c.to_digit(10));
-			let first_char = line.next().unwrap().to_string();
-			let last_char = line.last();
-			if last_char.is_some(){
-				return  (first_char + &last_char.unwrap().to_string()).parse::<u32>().unwrap();
-			} else {
-				return (first_char.repeat(2)).parse::<u32>().unwrap();
-			}
+			let first_char = line.next().unwrap();
+			let last_char = line.last().unwrap_or(first_char);
+			(first_char * 10) + last_char
 		}).sum()
 }
 
