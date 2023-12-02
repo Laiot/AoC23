887a6202e80629654bc73a4d751881c9fff90dd6
Carmelo Sarta <carmelo.sarta.main@gmail.com>
2023-12-02 07:31:35 +0100

    Day 1 Part 1

--- src/day1.rs
+++ src/day1.rs
@@ -1,11 +1,21 @@
 use aoc_runner_derive::aoc;
 
 #[aoc(day1, part1)]
-pub fn part1(_: &str) -> String{
-    String::from("Hello, world!")
-}
+pub fn part1(input: &str) -> u32{
+    input.lines()
+		.map(|line| {
+			let mut line = line.chars().filter_map(|c| c.to_digit(10));
+			let first_char = line.next().unwrap().to_string();
+			let last_char = line.last();
+			if last_char.is_some(){
+				return  (first_char + &last_char.unwrap().to_string()).parse::<u32>().unwrap();
+			} else {
+				return (first_char.repeat(2)).parse::<u32>().unwrap();
+			}
+		}).sum()
+	}
 
 #[aoc(day1, part2)]
-pub fn part2(_: &str) -> String {
-    String::from("Hello, world!")
+pub fn part2(input: &str) -> u32 {
+    0
 }
